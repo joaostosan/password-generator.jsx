8745caa7db4b8dde4d0ab73c40edc350faa69fec
Joao <joaov.dsantos@outlook.com>
2024-05-22 01:07:53 -0300

    Include core files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Container from "./components/Container";
+import Title from "./components/Title";
+
+export default function App() {
+
+  return (
+    <>
+      <Container
+      title="Password Generator"
+      />
+    </>
+  )
+}
+
